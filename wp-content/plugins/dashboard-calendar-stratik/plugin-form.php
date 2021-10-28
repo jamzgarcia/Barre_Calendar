eb2bcf9d306c191af764218cb96f48f23121a39d
Diego_Beltran <dbeltranpereira@gmail.com>
2021-10-28 15:12:02 -0500

    Cambiando icono del plugin

--- wp-content/plugins/dashboard-calendar-stratik/plugin-form.php
+++ wp-content/plugins/dashboard-calendar-stratik/plugin-form.php
@@ -12,7 +12,8 @@ require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
 require(ABSPATH . "wp-content/plugins/dashboard-calendar-stratik/admin/controller/code128.php");
 include(ABSPATH . 'wp-content/plugins/dashboard-calendar-stratik/admin/controller/classFunction.php');
 
-function EnablePluginForm(){
+function EnablePluginForm()
+{
   global $wpdb;
 
   $sqlstudent = "CREATE TABLE IF NOT EXISTS {$wpdb->prefix}dash_student(
@@ -24,8 +25,8 @@ function EnablePluginForm(){
       dash_student_tipo_estudiante VARCHAR(20) NULL, 
       PRIMARY KEY (dash_student_id) 
   );";
-  $createstudent = dbDelta($sqlstudent,true);
-  
+  $createstudent = dbDelta($sqlstudent, true);
+
   $sqlcoach = "CREATE TABLE IF NOT EXISTS {$wpdb->prefix}dash_coach(
       dash_coach_id INT(11) NOT NULL AUTO_INCREMENT,
       dash_coach_nombre INT(11) NOT NULL,
@@ -34,8 +35,8 @@ function EnablePluginForm(){
       dash_coach_fecha_nacimiento VARCHAR(20) NULL,
       PRIMARY KEY (dash_coach_id)
   );";
-  $createcoach = dbDelta($sqlcoach,true);
-  
+  $createcoach = dbDelta($sqlcoach, true);
+
   $sqlsede = "CREATE TABLE IF NOT EXISTS {$wpdb->prefix}dash_sede(
       dash_sede_id INT(11) NOT NULL AUTO_INCREMENT,
       dash_sede_nombre INT(11) NOT NULL,
@@ -43,7 +44,7 @@ function EnablePluginForm(){
       dash_sede_telefono VARCHAR(20) NULL,
       PRIMARY KEY (dash_sede_id)
   );";
-  $createsede = dbDelta($sqlsede,true);
+  $createsede = dbDelta($sqlsede, true);
 
   $sqlClass = "CREATE TABLE IF NOT EXISTS wp_dash_class( dash_class_id INT(11) NOT NULL AUTO_INCREMENT, 
   dash_student_id INT(11) NOT NULL, 
@@ -56,7 +57,7 @@ function EnablePluginForm(){
   FOREIGN KEY (dash_coach_id) REFERENCES wp_dash_coach(dash_coach_id) ON DELETE CASCADE, 
   FOREIGN KEY (dash_sede_id) REFERENCES wp_dash_sede(dash_sede_id) ON DELETE CASCADE, 
   FOREIGN KEY (id_user) REFERENCES wp_users(ID) ON DELETE CASCADE)";
-$createClass = dbDelta($sqlClass,true);
+  $createClass = dbDelta($sqlClass, true);
 
 
 
@@ -71,11 +72,11 @@ $createClass = dbDelta($sqlClass,true);
       FOREIGN KEY (dash_class_id) 
           REFERENCES wp_dash_class(dash_class_id) ON DELETE CASCADE
   );";
-$createCalendar = dbDelta($sqlCalendar,true);
+  $createCalendar = dbDelta($sqlCalendar, true);
 
 
 
-$sqlreserva = "CREATE TABLE IF NOT EXISTS wp_dash_reserva(
+  $sqlreserva = "CREATE TABLE IF NOT EXISTS wp_dash_reserva(
   dash_reserva_id INT(11) NOT NULL AUTO_INCREMENT,
   dash_student_id INT(11) NOT NULL,
   dash_coach_id INT(11) NOT NULL,
@@ -92,12 +93,11 @@ $sqlreserva = "CREATE TABLE IF NOT EXISTS wp_dash_reserva(
   FOREIGN KEY (id_user) 
       REFERENCES wp_users(ID) ON DELETE CASCADE   
 );";
-$createClass = dbDelta($sqlreserva,true);
-
-  
+  $createClass = dbDelta($sqlreserva, true);
 }
 
-function DisablePluginForm(){
+function DisablePluginForm()
+{
   global $wpdb;
   $sqlDelete = "DROP TABLE {$wpdb->prefix}dash_class, {$wpdb->prefix}dash_calendar, {$wpdb->prefix}dash_student, {$wpdb->prefix}dash_coach, {$wpdb->prefix}dash_sede, {$wpdb->prefix}dash_reserva;";
   $wpdb->query($sqlDelete);
@@ -110,7 +110,7 @@ register_deactivation_hook(__FILE__, "DisablePluginForm");
 add_action("admin_menu", "CreateMenu");
 function CreateMenu()
 {
-  add_menu_page("Dashboard Calendar", "Calendar Home", "manage_options", root_file . "dashboard_admin.php", null, plugin_dir_url(__file__) . "admin/images/icon.png", "4");
+  add_menu_page("Dashboard Calendar", "Calendar Home", "manage_options", root_file . "dashboard_admin.php", null, plugin_dir_url(__file__) . "admin/images/iconStratik.png", "4");
 }
 
 function bootstrap_css($hook)
@@ -154,13 +154,14 @@ add_action('admin_enqueue_scripts', 'EncolarJS');
 
 // shotrcodes 
 
-function viewDashboardAdmin(){
+function viewDashboardAdmin()
+{
   $adminData = new adminQuestions();
   $html = $adminData->layoutDashboardAdmin();
   return $html;
 }
 
-add_shortcode( "view_dashboard_admin", "viewDashboardAdmin" );
+add_shortcode("view_dashboard_admin", "viewDashboardAdmin");
 
 
 function add_styles_page()
@@ -170,9 +171,9 @@ function add_styles_page()
     wp_enqueue_style('bootstrap_css', plugins_url('admin/css/bootstrap/css/bootstrap.min.css', __FILE__));
     wp_enqueue_style('font_awesome_css', plugins_url('admin/css/font-awesome/css/font-awesome.min.css', __FILE__));
     wp_enqueue_style('asap_font', "https://fonts.googleapis.com/css?family=Didact+Gothic");
-    wp_enqueue_style('animated_css', "https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css");    
+    wp_enqueue_style('animated_css', "https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css");
     wp_enqueue_style('toaster_css', "//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css");
-    wp_enqueue_style('datatables_css', "https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css");    
+    wp_enqueue_style('datatables_css', "https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css");
     wp_enqueue_style('custom_css', plugins_url('admin/css/view_dashboard_admin.css', __FILE__));
     wp_enqueue_script('jquery_js', "https://code.jquery.com/jquery-3.4.1.min.js", array('jquery'));
     wp_enqueue_script('pdfobject_js', "https://cdnjs.cloudflare.com/ajax/libs/pdfobject/2.1.1/pdfobject.min.js", array('jquery'));
@@ -180,7 +181,7 @@ function add_styles_page()
     wp_enqueue_script('bootstrap_js', plugins_url('admin/css/bootstrap/js/bootstrap.min.js', __FILE__), array('jquery'));
     wp_enqueue_script('toaster_js', "//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js", array('jquery'));
     wp_enqueue_script('datatables_js', "https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js", array('jquery'));
-    wp_enqueue_script('datatablesjquery_js', "https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js", array('jquery')); 
+    wp_enqueue_script('datatablesjquery_js', "https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js", array('jquery'));
     wp_enqueue_script('validators_js', plugins_url('admin/js/validators.js', __FILE__), array('jquery'));
     wp_enqueue_script('scriipt_js', plugins_url('admin/js/scripts.js', __FILE__), array('jquery'));
     wp_enqueue_script('request_js', plugins_url('admin/js/request.js', __FILE__), array('jquery'));
